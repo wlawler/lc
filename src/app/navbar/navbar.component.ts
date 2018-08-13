c19a17e7e6ad84bcac51a78458a52dd41811b87d
wes lawler <wes.lawler2@gmail.com>
2018-08-12 19:52:47 -0500

    added dropdown menu to nav bar. Good, but could be better

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers:[NgbDropdown]
 })
 export class NavbarComponent implements OnInit {
 
